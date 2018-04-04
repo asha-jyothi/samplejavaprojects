8956b485c5ad031ace8dd31681703579d5fa29e7
asha <asha@madebyfire.com>
2018-04-04 14:53:08 +0530

    sample maven project

---
+++ src/main/java/com/samplemaven/calculation/Calculations.java
@@ -0,0 +1,18 @@
+package com.samplemaven.calculation;
+
+public class Calculations {
+
+    public static void main(String[] args) {
+        
+        int a =6;
+        int b = 3;
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        int f = a/b;
+        System.out.println("Addition-->"+c);
+        System.out.println("Subtraction-->"+d);
+        System.out.println("Multiplication-->"+e);
+        System.out.println("Division-->"+f);
+    }
+}
